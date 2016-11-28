0fca0bdbdb997f90e94d6d12378f1c3f0bac96d6
EugeneRuzanov <eugene.ruzanov@gmail.com>
2016-11-29 00:41:12 +0300

    - корзина

---
+++ js/app.js
@@ -0,0 +1,26 @@
+(function () {
+  var card = {};
+  var cardCount = $('[data-ui="card-count"]');
+  var template = _.template($('#widget-selection').html());
+  var widgetSelectionSale = $('[data-section="widget-selection-sale"]');
+  var widgetSelectionNew = $('[data-section="widget-selection-new"]');
+  var widgetSelectionTop = $('[data-section="widget-selection-top"]');
+  widgetSelectionSale.html(template({
+    items: products.filter(function (item) {return item.sale;})
+  }));
+  widgetSelectionNew.html(template({
+    items: products.filter(function (item) {return item.isNew;})
+  }));
+  widgetSelectionTop.html(template({
+    items: products.filter(function (item) {return item.isTop;})
+  }));
+  $('[data-ui="to-card"]').click(function () {
+    var productId = $(this).parent('[data-id]').data('id');
+    if (card[productId]) {
+      card[productId] = {count: card[productId] + 1}
+    } else {
+      card[productId] = {count: 0}
+    }
+    cardCount.html(+cardCount.text() + 1);
+  });
+})();
---
+++ js/data-products.js
@@ -0,0 +1,94 @@
+var products = [
+  // sale
+  {
+    id: 1,
+    sale: 10,
+    image: 'images/prod1.jpg',
+    title: 'Бордюр Коллаж мозаик на бел син 500*60*8,5 бд57кл003',
+    oldPrice: 3267,
+    price: 2940.30
+  },
+  {
+    id: 2,
+    sale: 8,
+    image: 'images/prod2.jpg',
+    title: 'Дрель сетевая безударная',
+    oldPrice: 3267,
+    price: 3005.64
+  },
+  {
+    id: 3,
+    sale: 23,
+    image: 'images/prod3.png',
+    title: 'Бордюр Коллаж мозаик на бел син 500*60*8,5 бд57кл003',
+    oldPrice: 3267,
+    price: 2515.59
+  },
+  {
+    id: 4,
+    sale: 15,
+    image: 'images/prod4.jpg',
+    title: 'Краска для стен Профилюкс, 3 кг',
+    oldPrice: 3267,
+    price: 2776.95
+  },
+  // new
+  {
+    id: 5,
+    isNew: true,
+    image: 'images/prod1.jpg',
+    title: 'Бордюр Коллаж мозаик на бел син 500*60*8,5 бд57кл003',
+    price: 3267
+  },
+  {
+    id: 6,
+    isNew: true,
+    image: 'images/prod2.jpg',
+    title: 'Дрель сетевая безударная',
+    price: 3267
+  },
+  {
+    id: 7,
+    isNew: true,
+    image: 'images/prod3.png',
+    title: 'Бордюр Коллаж мозаик на бел син 500*60*8,5 бд57кл003',
+    price: 3267
+  },
+  {
+    id: 8,
+    isNew: true,
+    image: 'images/prod4.jpg',
+    title: 'Краска для стен Профилюкс, 3 кг',
+    price: 3267
+  },
+  // top
+  {
+    id: 9,
+    isTop: true,
+    image: 'images/prod1.jpg',
+    title: 'Бордюр Коллаж мозаик на бел син 500*60*8,5 бд57кл003',
+    price: 3267
+  },
+  {
+    id: 10,
+    isTop: true,
+    image: 'images/prod2.jpg',
+    title: 'Дрель сетевая безударная',
+    price: 3267
+  },
+  {
+    id: 11,
+    isTop: true,
+    image: 'images/prod3.png',
+    title: 'Бордюр Коллаж мозаик на бел син 500*60*8,5 бд57кл003',
+    price: 3267
+  },
+  {
+    id: 12,
+    isTop: true,
+    image: 'images/prod4.jpg',
+    title: 'Краска для стен Профилюкс, 3 кг',
+    price: 3267
+  }
+
+];
